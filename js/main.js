aa6c18f998ef7f17b81480baf700192ecdfcc102
SimoneBenato <sb270298@gmail.com>
2021-07-20 15:53:48 +0200

    Stampa della lista in ordine alfabetico

---
+++ js/main.js
@@ -0,0 +1,14 @@
+// 1. chiedi all’utente il cognome
+var cognome = prompt("inserisci il tuo cognome")
+console.log(cognome)
+
+// 2. inseriscilo in un array con altri cognomi: ‘Bianchi’, ‘Neri’, ‘Rossi’, ‘Verdi’, ‘Gialli’
+var indiziati = ["Bianchi", "Neri", "Rossi", "Verdi", "Gialli"];
+indiziati.push(cognome);
+console.log(indiziati);
+
+// 3. stampa la lista ordinata alfabeticamente
+
+document.write(indiziati.sort());
+
+// 4. scrivi anche la posizione "umana" (partendo da 1) della lista in cui il nuovo utente si trova
